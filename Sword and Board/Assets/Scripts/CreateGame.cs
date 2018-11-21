c75a9a762ac765352689b876af9ea01b154e6482
m-bt <michalbiernat@windowslive.com>
2018-11-21 22:56:42 +0100

    Add: MainMenu and UNet Matchmaking
    Started implementing MainMenu and configured UNet Matchmaking serivces.

---
+++ Sword and Board/Assets/Scripts/CreateGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CreateGame : MonoBehaviour
+{
+    private NetworkManager networkManager;
+
+    private uint roomSize = 10;
+
+    public string RoomName { get; set; }
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (!networkManager.matchMaker) networkManager.StartMatchMaker();
+    }
+
+    public void CreateRoom()
+    {
+        if(RoomName != null && RoomName != "")
+        {
+            networkManager.matchMaker.CreateMatch(RoomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
